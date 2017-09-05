d99b30b31fe88dad0fe3014608c0259e66cba1e4
ViridanForge <viridianforge@gmail.com>
2017-09-04 22:05:41 -0400

    Adding the rough logic to automatically scrape a BC page for album metadata. Needs safety checks, and more flexibility.

--- autoWINdexer.py
+++ autoWINdexer.py
@@ -36,52 +36,52 @@ import requests
 # TODO
 
 dataLoc = {
-	'bcFullArts': 'https://bandcamp.com/artist_index',
-	'bcSearchQ': 'https://bandcamp.com/search',
-	'bcTagQ': 'https://bandcamp.com/tag/',
-	'bcTagsQ': 'https://bandcamp.com/tags'
+    'bcFullArts': 'https://bandcamp.com/artist_index',
+    'bcSearchQ': 'https://bandcamp.com/search',
+    'bcTagQ': 'https://bandcamp.com/tag/',
+    'bcTagsQ': 'https://bandcamp.com/tags'
 }
 
 #Functions
 
 #Main driver here
 def collateAlbums(dataLocs, queries, options):
-	"""
-	Function that pulls together all the data from a query
-	
-	The goal of this function is to grab all relevant data from the list
-	of services given, for the list of tags given.
+    """
+    Function that pulls together all the data from a query
+    
+    The goal of this function is to grab all relevant data from the list
+    of services given, for the list of tags given.
 
-	Parameters
-	----------
-	dataLocs : the Data Locations to query for data
-	queries: the queries to run on each dataLocations
-	options: any options to modify the queries with
-	"""
+    Parameters
+    ----------
+    dataLocs : the Data Locations to query for data
+    queries: the queries to run on each dataLocations
+    options: any options to modify the queries with
+    """
 
-	for url in dataLocs:
-		albumURLs = []
-		for query in queries:
-			relatedTags = []
-			curPageURLs = []
-			for page in range(1,10):
-				#Begin by querying the main location for target URLs
-				sourceData = requests.get(url + query + '?page='+ str(page) + '&sort_field=pop')
-				#Load the target URL's text data
-				txData = sourceData.text
-				soup = BeautifulSoup(txData,"html.parser")
-				#Grab what we need
-				if page==1:
-					rTags = soup.find_all("a", class_="related_tag")
-					print("Printing current Related Tags!")
-					for tag in rTags:
-						print(tag.text,"\n")
-						relatedTags.append(tag.text)
-				albumElems = soup.find_all("li", class_=["item", "item_end"])
-				for album in albumElems:
-					albumURLs.append(album.find("a").get("href"))
-	#Return the awesome stuff
-	return albumURLs
+    for url in dataLocs:
+        albumURLs = []
+        for query in queries:
+            relatedTags = []
+            curPageURLs = []
+            for page in range(1,10):
+                #Begin by querying the main location for target URLs
+                sourceData = requests.get(url + query + '?page='+ str(page) + '&sort_field=pop')
+                #Load the target URL's text data
+                txData = sourceData.text
+                soup = BeautifulSoup(txData,"html.parser")
+                #Grab what we need
+                if page==1:
+                    rTags = soup.find_all("a", class_="related_tag")
+                    print("Printing current Related Tags!")
+                    for tag in rTags:
+                        print(tag.text,"\n")
+                        relatedTags.append(tag.text)
+                albumElems = soup.find_all("li", class_=["item", "item_end"])
+                for album in albumElems:
+                    albumURLs.append(album.find("a").get("href"))
+    #Return the awesome stuff
+    return albumURLs
 
 def collateAlbum(albumURL, fields):
     """
@@ -97,22 +97,62 @@ def collateAlbum(albumURL, fields):
     txtData = sourceData.text
     soup = BeautifulSoup(txtData, "html.parser")
 
+    albumData = {}
+
+    if "tracks" in fields:
+        #Pull track data here
+        trackList = soup.find_all("tr", class_=["track_row_view"])
+        trackData = []
+        for track in trackList:
+            curTrack = {}
+            curTrack['number'] = track.find("div", class_=["track_number"]).text
+            curTrack['name'] = track.find("span", itemprop=["name"]).text
+            curTrack['lyrics'] = ''
+            duration = track.find("span", class_=["time"]).text
+            curTrack['duration'] = duration.strip()
+            trackData.append(curTrack)
+        
+        albumData['tracks'] = trackData
+        
+    if "artist" in fields:
+        #Pull artist data here
+        artistData = soup.find("span", itemprop=["byArtist"])
+        albumData['artist'] = artistData.find("a").text
+
+    if "release" in fields:
+        #Pull release date here
+        releaseDate = soup.find('meta',itemprop=['datePublished'])
+        albumData['release']=releaseDate['content']
+
+    if "tags" in fields:
+        #Pull album tags here
+        alTagElems = soup.find_all("a", class_=["tag"])
+        tags = []
+        for tag in alTagElems:
+            tags.append(tag.text)
+        albumData['tags'] = tags
+
+    print(albumData)
+    return albumData
+
 
 '''
 Main method documentation
 '''
 def main():
-	#Input Grabber here Eventually
-	#Logger here Eventually
-	#Temp for testing
-	result = collateAlbums(['https://bandcamp.com/tag/'],['chiptune'],0)
-	print("Results:")
-	for res in result:
-                collateAlbum(result)
-		print(res,"\n")
-	print("Done.")
-	return 0
+    #Input Grabber here Eventually
+    #Logger here Eventually
+    #Temp for testing
+    result = collateAlbums(['https://bandcamp.com/tag/'],['chiptune'],0)
+    print("Results:")
+    for res in result:
+        #collateAlbum(result)
+        print(res,"\n")
+    print("Testing Album Dive")
+    collateAlbum(result[0],['tracks','artist','release','tags'])
+    print("Done.")
+    return 0
 
 if __name__ == '__main__':
-	main()
+    main()
 
