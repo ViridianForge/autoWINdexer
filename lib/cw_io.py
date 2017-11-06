bd35fcc1fa4c169829acc93a9bf1f7f0b93c86cf
ViridanForge <viridianforge@gmail.com>
2017-11-06 01:48:27 -0500

    Fleshing out delimited output for the io library.

--- lib/cw_io.py
+++ lib/cw_io.py
@@ -6,7 +6,7 @@
     a method for each popular IO method.
 """
 import json
-
+import csv
 
 def cw_out(func, data, opts):
     """
@@ -27,8 +27,32 @@ def cw_out(func, data, opts):
     except:
         print("Unexpected error:", sys.exc_info()[0])
 
-def cw_csv(data, opts):
-    return 0
+def cw_delim(data, opts):
+    """
+    Generic Delimited Text Output
+    Print out data to file in opt[0]
+    Default to csv, quoted with | and quote minimal behavior
+
+    Parameters:
+    ________________________
+    data: The data to output
+    opts: Options to pass to function
+    opt[0]: Filename to write to
+    opt[1]: File mode
+    opt[2]: Delimiter to use in output - defaults to ,
+    opt[3]: Quote character to surrounded delimited strings - defaults to |
+    opt[4]: Quoting Behavior constant - defaults to minimal
+    """
+    delim = opts[2] or ","
+    quote = opts[3] or "|"
+    quotopt = opts[4] or csv.QUOTE_MINIMAL
+    try:
+        with open(opts[0], opts[1]) as outfile:
+            cw_writer = csv.writer(outfile, delimiter=delim, quotechar=quote, quoting=quotopt)
+            for row in data:
+                cw_writer.writerow(row)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
 
 def cw_json(data, opts):
     """
